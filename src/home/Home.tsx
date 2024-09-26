0c7f6dfb161faae9c0ac65799365549fe5dad5da
yntymak <kubanychevyntymak21@gmail.com>
2024-09-27 00:52:23 +0600

    - branch

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -5,6 +5,14 @@ const Home = () => {
     <div>
       Home
       <h1>YNTY</h1>
+      <h1>DEVELOPER</h1>
+      <h1>IT</h1>
+      <button>CLick</button>
+      <button>CLick</button>
+      <button>CLick</button>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
     </div>
   );
 };
