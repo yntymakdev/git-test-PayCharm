f6235e6f3b52b8deb500b91adc30efbe824d270b
yntymak <kubanychevyntymak21@gmail.com>
2024-09-27 12:32:11 +0600

    qw

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -39,6 +39,28 @@ const Home = () => {
       <h1>ynty.io</h1>
       <h1>ynty.io</h1>
       <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <h1>ynty.io</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <br />
     </div>
   );
